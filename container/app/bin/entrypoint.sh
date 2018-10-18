b1358d6d56c77b6da80e401be14daf6c7e8bb01c
Himadri Singh <hsingh@quid.com>
2018-10-18 17:07:33 +0530

    Refactored into two diff containers

--- container/app/bin/entrypoint.sh
+++ container/app/bin/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-echo "Starting http-prophet..."
-/usr/bin/python /app/http-prophet.py  &
-
 envsubst < /app/conf/sherlock.properties.template > /app/conf/sherlock.properties
 
-/jdk/bin/java $(cat conf/jvm.config | xargs) \
+cat /app/conf/sherlock.properties
+
+java $(cat conf/jvm.config | xargs) \
     -cp $(ls lib/sherlock-*-selfcontained.jar | xargs | tr ' ' ':') \
     com.yahoo.sherlock.App --config /app/conf/sherlock.properties
